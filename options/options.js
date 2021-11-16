e04e3701f1c71640a7a7b847163696650ebc7117
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-16 04:12:02 -0700

    Update options.js

--- options/options.js
+++ options/options.js
@@ -2,8 +2,8 @@ import { StyleSheet } from "react-native";
 
 export const globalOptions = {
   name: "demo_app_31894",
-  url: "https://demo_app_31894.botics.co",
-  api: "https://demo_app_31894.botics.co/api/v1"
+  url: "https://demo-app-31894.botics.co",
+  api: "https://demo-app-31894.botics.co/api/v1"
 }
 export const modulesOptions = {
   "@modules/app-menu": {
